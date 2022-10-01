f8ef5cb54b0936e8c316f70a5d7190c489bd1406
EyuaelB <eyuaelbezabeh@gmail.comlear>
2022-10-01 07:36:28 +0300

    with visual data

--- web/about_data.py
+++ web/about_data.py
@@ -1,7 +1,17 @@
 import pandas as pd
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 import streamlit as st
 
 
+def data_for_bar_chart(df):
+    data = df.groupby(['day'])['income'].mean().sort_values(ascending=True)
+    return data
 
-df = pd.read_csv('../shilmat_data.csv')
+
+def about_data():
+    st.title("መዝገቡን በ እይታ ይረዱ")
+ 
+    
+    df = pd.read_csv('shilmat_data.csv')
+    st.bar_chart(data_for_bar_chart(df))
+
--- web/app.py
+++ web/app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from model_trial_page import model_try_out_page
+from about_data import about_data
 
-st.sidebar.selectbox("Try or View Data", ("Try-Out", "View Data"))
 
-model_try_out_page()
+selected = st.sidebar.selectbox("Try or View Data", ("Try-Out", "View Data"))
+
+if selected == 'View Data':
+    about_data()
+else:
+    model_try_out_page()
