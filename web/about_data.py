6732ad7c3fd5b85b7db6f2a5a996f9354d6ded8e
EyuaelB <eyuaelbezabeh@gmail.comlear>
2022-10-01 06:56:03 +0300

    clean_up

--- web/about_data.py
+++ web/about_data.py
@@ -1,13 +1,7 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-<<<<<<< HEAD
-
-
-
-df = pd.read_csv
-=======
 import streamlit as st
 
 
+
 df = pd.read_csv('../shilmat_data.csv')
->>>>>>> a376866c4d8dc619477aea785cb0ee811b23e74e
