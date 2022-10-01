da707dc93354e68521fa3dafdd23d983adf50a6c
EyuaelB <eyuaelbezabeh@gmail.comlear>
2022-10-01 06:17:38 +0300

    try out page working

---
+++ web/about_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import streamlit as st
+
+
+df = pd.read_csv('../shilmat_data.csv')
--- web/app.py
+++ web/app.py
@@ -1,5 +1,6 @@
-import streamlit
+import streamlit as st
 from model_trial_page import model_try_out_page
 
+st.sidebar.selectbox("Try or View Data", ("Try-Out", "View Data"))
 
 model_try_out_page()
