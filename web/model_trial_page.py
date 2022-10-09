7c050d89caf83ce0040d85675ef4888d7f0fc56b
EyuaelB <eyuaelbezabeh@gmail.comlear>
2022-10-09 16:41:22 +0300

    updated model try out

--- web/model_trial_page.py
+++ web/model_trial_page.py
@@ -2,17 +2,20 @@ import numpy as np
 import streamlit as st
 import tools
 
-#with old csv format 
+
 # def model_try_out_page():
 #     st.title("የ፪ ሺህ ሓበሻ የባህል ምግብ አዳራሽ የሙዚቃ ባንድ የዕለት ሽልማት መተንበያ ድረ-ገፅ")
 #     st.write("""### የሽልማት ትንበያ ቀኑን ያስገቡ""")
 
-#     day = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    # day = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
-#     day = st.selectbox("ቀን", day)
+    # day = st.selectbox("ቀን", day)
+    # day_of_month = 
+    # year = 
 
-#     clicked = st.button("ተንብይ")
-
+    # clicked = st.button("ተንብይ")
+    
+#with old csv format 
 #     if clicked:
 #         data = tools.load_model_data('current_model_19_02_test.pkl')
 #         model = tools.get_model(data)
@@ -27,50 +30,75 @@ import tools
         
 #         st.subheader(f'የሽልማት መጠን {y_pred[0]:.2f}')
   
+   
+
+#with new csv format
+    # if clicked:
+    #     data = tools.load_model_data('xgmodel27.pkl')
+    #     model = tools.get_model(data)
+    #     le_day = tools.get_le_day(data)
+        
+        
+    #     #input array
+    #     np.array([[]])
+        
+        
+        
+    #     input = np.array([[day.lower()]])
+    #     input[:, 0] = le_day.transform(input[:, 0])
+        
+    #     input = input.astype(float)
+        
+    #     y_pred = model.predict(input)
+        
+    #     st.subheader(f'የሽልማት መጠን {y_pred[0]:.2f}')
+
+
+
     
 def model_try_out_page():
     st.title("የ፪ ሺህ ሓበሻ የባህል ምግብ አዳራሽ የሙዚቃ ባንድ የዕለት ሽልማት መተንበያ ድረ-ገፅ")
     st.write("""### የሽልማት ትንበያ ቀኑን ያስገቡ""")
-
-    day_opt = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     
-    input_date = st.date_input("ቀን")
+    
+    # First Input
+    day_opt = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    day = st.selectbox("ዕለት/Day Of The Week", day_opt)
+
+    
+    # Second input
+    input_day_of_month = st.slider('የወሩ ቀን/Day Of The Month',1,30,1)
+    
+    
+    # Third input
+    month_opt = ("Jan", "Feb", "March", "April", "May", "June", "July","Aug","Sep","Oct","Nov","Dec")
+    month = st.selectbox("ቀን/Day Of The Month", month_opt)
+    
+    
+    # Last Input
+    input_year = st.slider('ዓ.ዓ/Year',2022,2025,2022)
+    
+    
+    # input_date = st.date_input("ቀን")
   
     clicked = st.button("ተንብይ")
-
     if clicked:
-        data = tools.load_model_data('current_model_21_02_test.pkl')
+        data = tools.load_model_data('xgmodel27.pkl')
         model = tools.get_model(data)
-        le_day = tools.get_le_day(data)
-        le_date = tools.get_le_date(data)
-        le_month= tools.get_le_month(data)
-        le_year = tools.get_le_year(data)
-
-        input = np.array([[input_date.day,input_date.month,input_date.year,day_opt[input_date.weekday()].lower()]])
         
         
-        print(input[:,0])
-        input[:,0] = le_date.transform(input[:,0])
-        print(input[:,0])
-       
-        input[:,1] = le_month.transform(input[:,1])
-        print(input[:,1])
-       
-        input[:,2] = le_year.transform(input[:,2])
-        print(input[:,2])
-       
-        input[:,3] = le_day.transform(input[:,3])
-        print(input[:,3])        
-        
-        
-        input = input.astype(float)
-        
+        # input format -  DayName,DayOfTheWeek,DayOfTheMonth,Month,Year
+        input = np.array([[tools.feature_day_of_week(day,day_opt),
+                           input_day_of_month,
+                           tools.feature_day_of_month(month,month_opt),
+                           input_year]])
+              
         y_pred = model.predict(input)
         
-        # print(day_opt[input_date.weekday()].lower())
+        st.subheader(f'የሽልማት መጠን/Predicted Income {y_pred[0]:.2f}')
+
+   
         
-        st.subheader(f' {day_opt[input_date.weekday()]}, የሽልማት መጠን {y_pred[0]:.2f}')
-    
         
         
         
--- web/tools.py
+++ web/tools.py
@@ -24,3 +24,23 @@ def get_le_month(data):
 
 def get_le_year(data):
     return data['le_year']
+
+
+
+# def feature_day_name(day_name:str,day_opt:tuple)->int:
+#     for i in range(0,7):
+#         if day_opt[i] ==  day_name:
+#             return i
+
+
+def feature_day_of_week (day_name:str,day_opt:tuple)->int:
+    for i in range(0,7):
+        if day_name == day_opt[i]:
+            return i    
+    
+        
+def feature_day_of_month (day_of_month:str,month_opt:tuple)->int:
+    for i in range(1,31):
+        if  month_opt[i] == day_of_month:
+            return i+1    
+    
